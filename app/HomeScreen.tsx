2add7118cdfa11d06a34c8876142028ecc0e881a
adharsh <aarshuu777@gmail.com>
2025-02-03 00:24:07 +0530

    Initial commit of React Native project

---
+++ app/HomeScreen.tsx
@@ -0,0 +1,13 @@
+// HomeScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to the Home Screen!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;  // Make sure it's the default export
